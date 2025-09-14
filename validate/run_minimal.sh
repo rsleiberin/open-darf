89486834e01aae91e66d1452c813953aeb85d754
rsleiberin <rsleiberin@gmail.com>
2025-09-14 14:16:15 -0500

    chore(validate): export VALIDATOR_OUTDIR from runner for aligned receipts

--- validate/run_minimal.sh
+++ validate/run_minimal.sh
@@ -11,10 +11,13 @@ TS="$(date -u +%Y%m%dT%H%M%SZ)"
 OUTDIR="var/receipts/${TS}_validator"
 mkdir -p "$OUTDIR"
 
+export VALIDATOR_OUTDIR="$OUTDIR"
+
 set +e
 python3 validate/minimal_validator.py | tee "$OUTDIR/validator.console.out"
 VAL_RC=${PIPESTATUS[0]}
 set -e
+
 echo "${VAL_RC}" > "$OUTDIR/validator.rc"
 
 echo "[M3] validator rc=${VAL_RC}"
